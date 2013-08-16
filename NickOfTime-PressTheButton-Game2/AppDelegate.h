6c35afc4609a0fedb858b096e4e6a19fe3b84ddc
normlink <ngershon@outlook.com>
2013-08-16 07:51:49 -0500

    Initial Commit

---
+++ NickOfTime-PressTheButton-Game2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NickOfTime-PressTheButton-Game2
+//
+//  Created by Yaniv Kerem on 8/16/13.
+//  Copyright (c) 2013 Norm Gershon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NickOfTime-PressTheButton-Game2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NickOfTime-PressTheButton-Game2
+//
+//  Created by Yaniv Kerem on 8/16/13.
+//  Copyright (c) 2013 Norm Gershon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
